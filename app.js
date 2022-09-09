42100bfbd53b67fc928a00a7136ba9b6020cd5be
Ruslan Gaziev <grinrus@yandex.ru>
2022-09-09 19:05:30 +0300

    some comments added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const routerUsers = require('./routes/users');
 const routerMovies = require('./routes/movies');
 const NotFoundError = require('./errors/not-found-errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { corsOptions } = require('./utils/corsOptions');
+// const { corsOptions } = require('./utils/corsOptions');
 const { handleError } = require('./utils/handleError');
 const { limiter } = require('./utils/limiter');
 const { MONGODB_SERVER } = require('./utils/config');
@@ -18,7 +18,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors()); // включить опции после создания сервера и подключения домена
+app.use(cors()); // включить опции после создания фронта и подключения его к серверу
 
 app.use(limiter);
 
